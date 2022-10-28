6151302445d011d4e6631f117847989b4132937d
Elie Yumba <96975253+yumbaelie01@users.noreply.github.com>
2022-10-28 21:34:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+print("Hello World")
+
+print("more strings")
+print(4)
+
+#variabble
+
+a = 5
+print(a)
+
+b = "My name is:"
+print(b)
+
+print(a, b)
+
+e = b
+
+print(e)
+
+
+#swipping to variables
+
+v1 = "first string"
+v2 = "second string"
+
+y = v1
+r =  v2
+
+print(b)
+
+print(e)
+
+r2 = v1
+v1 = v2
+v2 = r2
+print(v1)
+
+print(v2)
+
